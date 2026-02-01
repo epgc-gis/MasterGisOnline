e925369a9bf71b9fae4636a9379259fb260ce03a
epgc-gis <elenapalaciosgcu@gmail.com>
2026-02-01 11:58:29 +0100

    Ejercicio1a

---
+++ JS/Ejercicio1a/main.js
@@ -0,0 +1,13 @@
+//Utilización de variables
+
+const lado1 = 16
+const lado2 = 23
+const altura= 13
+
+const volumenCubo = lado1 * lado2 * altura
+const areaCubo = (2*((lado1 * lado2 )+ (lado1 * altura) + (
+    lado2 *altura)))
+
+
+console.log (volumenCubo)
+console.log (areaCubo)
