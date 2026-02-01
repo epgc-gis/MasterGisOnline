8a63c1084b7e71b3665c2dba69e5caff1b1ff471
epgc-gis <elenapalaciosgcu@gmail.com>
2026-02-01 17:41:27 +0100

    Update main.js

--- JS/Ejercicio1g/main.js
+++ JS/Ejercicio1g/main.js
@@ -1,3 +1,5 @@
+// Ejercicio 1g.1
+
 const numeros = [1,2,3,4,5]
 
 function sumaNumerosPares(numeros){
@@ -12,3 +14,29 @@ function sumaNumerosPares(numeros){
 }
 
 console.log('Ejercicio 1g.1 =', sumaNumerosPares(numeros))
+
+//Ejercicio 1g.2
+
+const cadena = 'Hola Mundo'
+
+function eliminarConsonantes(cadenaInput){
+    let vocales =''
+    const cadenaArray = cadenaInput.toLowerCase().split ('')
+    
+    for(const letra of cadenaArray){
+        if(letra == 'a'|| letra == 'ae'|| letra == 'i'|| letra == 'o'|| letra == 'u'){
+            vocales += letra
+        }
+    }
+
+    return vocales
+}
+
+console.log ('Ejercicio 1g.2 =' , eliminarConsonantes(cadena))
+
+//Ejercicio 1g.3
+
+function CelsiusAFarenheit (gradosCelsius){
+    return (gradosCelsius *(9/5) +32)
+}
+console.log('Ejercicio 1g.3 =', CelsiusAFarenheit (20))
