45be17be2f88bfe9c5dd6ee8c5d6367999178531
epgc-gis <elenapalaciosgcu@gmail.com>
2026-02-02 18:10:40 +0100

    Update main.js

--- JS/Ejercicio1I/main.js
+++ JS/Ejercicio1I/main.js
@@ -7,3 +7,14 @@ console.log(penultimoEnlace)
 const tercerParrafo = document.getElementById('third-paragraph')
 const numeroEnlacesTercerParrafo = 
     tercerParrafo.getElementsByTagName('a').length
+
+
+
+const parrafoResultados = document.createElement('p');
+
+const contenidoParrafoResultados = document.createTextNode(
+  `El número de enlaces en la página es: ${enlaces.length}, el link al que apunta el penúltimo enlace es: ${penultimoEnlace} y el número de enlaces del tercer párrafo es ${numeroEnlacesTercerParrafo}`
+);
+
+parrafoResultados.appendChild(contenidoParrafoResultados);
+document.body.appendChild(parrafoResultados);
